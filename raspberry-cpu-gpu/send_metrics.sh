b29abcb9ff026865e68454d4b0d9fbb84cb5bf8a
Gabriel <loktwo@outlook.com>
2022-01-14 13:57:47 -0300

    send metrics

---
+++ raspberry-cpu-gpu/send_metrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while :
+do
+    gpu="$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6-)"
+    gpu_final="$(echo ${gpu:5:4})"
+    curl -si -XPOST "http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39" --data-binary "gpu,host=localhost,region=br value=${gpu_final}" > /dev/null
+
+    cpu=$(</sys/class/thermal/thermal_zone0/temp)
+    cpu_final=$((cpu/1000))
+    curl -si -XPORT "http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39" --data-binary "cpu,host=localhost,region=br value=${cpu_final}" > /dev/null
+    sleep 30
+done
