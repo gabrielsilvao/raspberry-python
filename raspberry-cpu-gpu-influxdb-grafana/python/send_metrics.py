cb983cd544ccf469c5e4d2c0007e934fa1aea7d1
Gabriel <loktwo@outlook.com>
2022-01-14 18:46:03 -0300

    send metrics in python

--- raspberry-cpu-gpu-influxdb-grafana/python/send_metrics.py
+++ raspberry-cpu-gpu-influxdb-grafana/python/send_metrics.py
@@ -9,12 +9,10 @@ try:
         temp = cpu.temperature
         pload = 'cpu,host=localhost,region=br value={0}'.format(temp)
         r = requests.post('http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39', data=pload)
-        print(r.text)
 
         gpu = os.popen('echo $(/opt/vc/bin/vcgencmd measure_temp | cut -c 6- | cut -c -4)').read()
         pload2 = 'gpu,host=localhost,region=br value={0}'.format(gpu)
-        r2 = requests.post('http://localhost:8086/write?db=sensor&u=sensor&p=seletiva39', data=pload2)
-        print(r2.text)
+        r2 = requests.post('http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39', data=pload2)
 
         time.sleep(15)
 
