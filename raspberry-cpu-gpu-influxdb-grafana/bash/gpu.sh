c11d645010fe4218b5bd90973bc94827d75b681b
loktwo <loktwo@outlook.com>
2022-02-01 11:18:47 -0300

    diretorio diferente no nacional

--- raspberry-cpu-gpu-influxdb-grafana/bash/gpu.sh
+++ raspberry-cpu-gpu-influxdb-grafana/bash/gpu.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gpu="$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6-)"
 echo ${gpu}
+
+# Dependendo do Raspberry, o arquivo de execucao podera estar em outro diretorio
+# gpu="$(/usr/bin/vcgencmd measure_temp | cut -c 6-)"
