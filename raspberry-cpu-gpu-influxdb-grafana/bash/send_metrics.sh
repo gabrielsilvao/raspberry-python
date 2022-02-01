745257aa8fd62e8984d380a2d3fbab6dfd5d6361
loktwo <loktwo@outlook.com>
2022-02-01 14:05:20 -0300

    send metrics

--- raspberry-cpu-gpu-influxdb-grafana/bash/send_metrics.sh
+++ raspberry-cpu-gpu-influxdb-grafana/bash/send_metrics.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 while :
 do
-    gpu="$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6-)"
-    gpu_final="$(echo ${gpu:5:4})"
-    curl -si -XPOST "http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39" --data-binary "gpu,host=localhost,region=br value=${gpu_final}" > /dev/null
+    gpu="$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6- | cut -c -4)"
+    curl -si -XPOST "http://localhost:8086/write?db=sensor&u=grafana&p=seletiva39" --data-binary "gpu,host=localhost,region=br value=${gpu}" > /dev/null
 
     cpu=$(</sys/class/thermal/thermal_zone0/temp)
     cpu_final=$((cpu/1000))
